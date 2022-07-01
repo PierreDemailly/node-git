479a853b4775db52cc157d63d6cbc622514eca1e
SofianD <doual.sofian@hotmail.com>
2022-07-01 20:50:38 +0200

    refactor(parse-commits.js): use regexp to get data from commits
    Now commander returns raw text instead of an array of strings.

--- commander.js
+++ commander.js
@@ -1,9 +1,13 @@
 import {exec} from 'node:child_process';
 
-export function commander(command) {
+export function commander(command, options = {}) {
   return new Promise(resolve => {
     exec(command, (_, stdout, stderr) => {
-      resolve(stdout ? stdout.split('\n') : stderr);
+      if (options.split) {
+        stdout = stdout.split('\n');
+      }
+
+      resolve(stdout || stderr);
     });
   });
 }
--- helpers/parse-commits.js
+++ helpers/parse-commits.js
@@ -25,36 +25,14 @@
  * }, ...]
  * ```
  */
-export function* $parseCommits(lines) {
-  const currentCommit = [];
+export function $parseCommits(text) {
+  const rawCommits = text.match(/commit.*?(?=commit\ |$)/gs);
+  const commits = [];
 
-  for (const line of lines) {
-    const length = currentCommit.length;
-    if (line.startsWith('commit') && length) {
-      const hash = currentCommit[0].split('commit ')[1];
-      // from "Author: PierreD <e@mail.com>" to ["Author: PierreD", "e@mail.com>"]
-      const authorStr = currentCommit[1].split(' <');
-      // remove "Author: "
-      const name = authorStr[0].split('Author: ')[1];
-      // need to slice without last character ">" of the email
-      const email = authorStr[1].slice(0, -1);
-      const date = currentCommit[2].split('Date:')[1].trimStart();
-      // currentCommit[3] is always a blank line
-      const message = currentCommit[4].trimStart();
-
-      yield {
-        hash,
-        author: {
-          name,
-          email,
-        },
-        date,
-        message,
-      };
-      yield* $parseCommits(lines.slice(length));
-
-      return;
-    }
-    currentCommit.push(line);
+  for(const rawCommit of rawCommits) {
+    const [input, commit, merged, author, date, msg] = rawCommit.match(/commit(.*?)(?:Merge(.*?))?Author(.*?)Date(.*?)\\n(.*?)$/s);
+    commits.push({ input, commit, merged, author, date, msg });
   }
+
+  return commits;
 }
