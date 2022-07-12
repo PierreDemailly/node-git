1cabcd6bf41184efceedbbd18968a7dc13a554c3
PierreD <39910767+PierreDemailly@users.noreply.github.com>
2022-07-12 14:10:03 +0200

    feat(git-api): add git-commit handler (#5)

---
+++ git-commit.js
@@ -0,0 +1,40 @@
+import { commander } from "./commander.js";
+
+/**
+ * Execute `git commit` command with the message `--message` (alias `-m`)
+ * flag for each given message.
+ * @example
+ * commit("hello") // exec. git commit -m "hello"
+ * commit(["hello", "world"]) // exec. git commit -m "hello" -m "world"
+ *
+ * @param {string|string[]} message
+ *
+ * @todo
+ * We may returns the number of indexed files before before the commit.
+ * But it would need to refactor the lib to be able to call "plugins" weither
+ * given options. Also it could be well to have multiple custom error implementations
+ * e.g: NotAGitRepositoryError, NoIndexedFileError, IdentityUnknownError...
+ */
+export async function commit(message) {
+  const stringifiedMessages = (Array.isArray(message) ? message : [message]).flatMap((msg) => {
+    if (!msg) {
+      return [];
+    }
+
+    if (typeof msg === "string") {
+      return `-m ${msg}`;
+    }
+
+    throw new TypeError(`Expected string or string[], got ${typeof msg}`);
+  }).join(" ");
+
+  if (!stringifiedMessages) {
+    throw new Error('No message given');
+  }
+
+  try {
+    await commander(`git commit ${stringifiedMessages}`);
+  } catch (error) {
+    throw error;
+  }
+}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { logs } from "./git-log.js";
+import { commit } from "./git-commit.js";
 
 export const NodeGit = Object.freeze({
-  logs
+  logs,
+  commit,
 });
