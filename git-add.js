58ce88446772585747888b119458ca5a2c18deb4
PierreDemailly <pierredemailly.pro@gmail.com>
2022-07-13 19:53:15 +0200

    feat(git-api): add git add handler

---
+++ git-add.js
@@ -0,0 +1,54 @@
+import { commander } from "./commander.js";
+
+/**
+ * Execute `git add` command.
+ *
+ */
+async function add(path) {
+  try {
+    await commander(`git add ${path}`);
+  }
+  catch (error) {
+    throw error;
+  }
+}
+
+export async function indexFileOrDirectory(path) {
+  if (!path) {
+    throw Error("Please provide a file or a directory to index.");
+  }
+
+  if (typeof path !== "string") {
+    throw new TypeError(`Expected string, got ${typeof path}.`);
+  }
+
+  await add(path);
+}
+
+export async function indexFilesOrDirectories(paths) {
+  if (!paths) {
+    throw Error("No file neither directory found.");
+  }
+
+  for (const path of paths) {
+    if (typeof path !== "string") {
+      throw new TypeError(`Expected string, got ${typeof path}.`);
+    }
+
+    await add(path);
+  }
+}
+
+export async function indexAll(options) {
+  if (options.withoutNewFiles) {
+    await add("-u");
+  }
+}
+
+export async function indexAllCurrentDirectory(options) {
+  if (options.ignoreRemovals) {
+    await add(". --ignore-removal");
+  }
+
+  await add(".");
+}
