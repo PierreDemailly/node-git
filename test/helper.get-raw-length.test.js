8a94675f27d6f07d7e3bd7d2ad971ce40650a0ef
PierreDemailly <pierredemailly.pro@gmail.com>
2022-07-28 18:11:38 +0200

    fix tests

--- test/helper.get-raw-length.test.js
+++ test/helper.get-raw-length.test.js
@@ -2,9 +2,9 @@ import { getRawLength } from "../helpers/get-raw-length.js";
 
 import tap from "tap";
 
-
 tap.equal(getRawLength("foo\nbar\n"), 2, "should have 2 raws");
 
-tap.equal(getRawLength(`one
-                        two
-                        three`), 3, "should have 3 rows");
+tap.equal(getRawLength(`
+    one
+    two
+    three`), 3, "should have 3 rows");
