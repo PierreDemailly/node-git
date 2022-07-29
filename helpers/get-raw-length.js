5033b7c0917731b3e7397066d5c635b036cd8755
PierreD <39910767+PierreDemailly@users.noreply.github.com>
2022-07-29 12:36:32 +0200

    feat(git-api): add git changes count handler (#8)
    * feat(git-api): add git changes count handler
    
    * test(helpers) add getRawLength tests
    
    * fix: regexp to get raw commits (#9)
    
    * feat(git-api): add git changes count handler
    
    * test(helpers) add getRawLength tests
    
    * fix tests
    
    * remove useless test
    
    * test(helpers) add tests for parseCommits
    
    * feat(git-api): add git changes count handler
    
    * test(helpers) add getRawLength tests
    
    * fix tests
    
    * make linter to pass
    
    * add github workflow
    
    Co-authored-by: Yefis <39944043+SofianD@users.noreply.github.com>

---
+++ helpers/get-raw-length.js
@@ -0,0 +1,13 @@
+/**
+ * Get number of lines separated with newlines "\n" in the string.
+ *
+ * @param {string} rawText
+ * @returns
+ */
+export function getRawLength(rawText) {
+  if ((typeof rawText !== "string")) {
+    throw new TypeError(`string param required. ${typeof rawText} given.`);
+  }
+
+  return rawText.split("\n").length - 1;
+}
