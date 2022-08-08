ce9a9de682abe6566e012c0243dffc824cc276bb
PierreDemailly <pierredemailly.pro@gmail.com>
2022-08-08 16:42:01 +0200

    feat(TS): add type declarations

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-import { add, indexAll, indexAllCurrentDirectory, indexFileOrDirectory, indexFilesOrDirectories } from "./git-add.js";
 import { changesCount, stagedCount } from "./git-changes.js";
+import { indexAll, indexAllCurrentDirectory, indexFileOrDirectory, indexFilesOrDirectories } from "./git-add.js";
 import { commit } from "./git-commit.js";
 import { logs } from "./git-log.js";
 import { push } from "./git-push.js";
 import { restoreFile } from "./git-restore.js";
 
 export {
-  add,
   changesCount,
   commit,
   indexAll,
