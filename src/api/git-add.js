8811086940a740c837795accce0a06fa9e3c3e5c
PierreDemailly <39910767+PierreDemailly@users.noreply.github.com>
2023-02-06 20:19:10 +0100

    refacto: add standard (#28)

--- src/api/git-add.js
+++ src/api/git-add.js
@@ -1,4 +1,4 @@
-import { commander } from "../commander.js";
+import { commander } from '../commander.js'
 
 /**
  * @typedef {import("./index").indexAll} IndexAllType
@@ -11,69 +11,64 @@ import { commander } from "../commander.js";
  * Execute `git add` command.
  *
  */
-async function add(path) {
-  try {
-    await commander(`git add ${path}`);
-  }
-  catch (error) {
-    throw error;
-  }
+async function add (path) {
+  await commander(`git add ${path}`)
 }
 
 /**
  * @see {@link indexFileOrDirectoryType}
  */
-export async function indexFileOrDirectory(path) {
+export async function indexFileOrDirectory (path) {
   if (!path) {
-    throw Error("Please provide a file or a directory to index.");
+    throw Error('Please provide a file or a directory to index.')
   }
 
-  if (typeof path !== "string") {
-    throw new TypeError(`Expected string, got ${typeof path}.`);
+  if (typeof path !== 'string') {
+    throw new TypeError(`Expected string, got ${typeof path}.`)
   }
 
-  await add(path);
+  await add(path)
 }
 
 /**
  * @see {@link indexFilesOrDirectoriesType}
  */
-export async function indexFilesOrDirectories(paths) {
+export async function indexFilesOrDirectories (paths) {
   if (!Array.isArray(paths)) {
-    throw TypeError(`Expected array, got ${typeof paths}.`);
+    throw TypeError(`Expected array, got ${typeof paths}.`)
   }
 
   if (!paths) {
-    throw Error("No file neither directory given.");
+    throw Error('No file neither directory given.')
   }
 
   for (const path of paths) {
-    await indexFileOrDirectory(path);
+    await indexFileOrDirectory(path)
   }
 }
 
 /**
  * @see {@link IndexAllType}
  */
-export async function indexAll(options) {
+export async function indexAll (options) {
   if (options.omitNewFiles) {
-    await add("-u");
+    await add('-u')
 
-    return;
+    return
   }
 
-  await add("--all");
+  await add('--all')
 }
 
 /**
  * @see {@link indexAllCurrentDirectoryType}
  */
-export async function indexAllCurrentDirectory(options) {
+export async function indexAllCurrentDirectory (options) {
   if (options.ignoreRemovals) {
-    await add(". --ignore-removal");
+    await add('. --ignore-removal')
 
-    return;
+    return
   }
 
-  await add(".");
+  await add('.')
 }
