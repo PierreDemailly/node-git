266fe422da19827c611b4ac27fc7039784e95efe
PierreDemailly <pierredemailly.pro@gmail.com>
2023-03-22 16:29:29 +0100

    fix: remove currentBranch EOL

--- src/api/git-current-branch.js
+++ src/api/git-current-branch.js
@@ -3,5 +3,5 @@ import { commander } from '../commander.js'
 export async function currentBranch () {
   const rawText = await commander('git rev-parse --abbrev-ref HEAD')
 
-  return rawText
+  return rawText.replace('\n', '')
 }
